73f1b97709a193702c3fe7f175dbd935b141c447
Kundan Bhardwaj <kundanbhardwaj145@gmail.com>
2021-05-21 21:51:42 +0530

    second commit

--- dj1/views.py
+++ dj1/views.py
@@ -1,4 +1,7 @@
 #first file
 from django.http import HttpResponse
+from django.shortcuts import render
 def index(requests):
-	return HttpResponse('''<h1 these are some important links </h1> <a href = "https://www.remove.bg/"> click to remove background</a>>''')
+	return render(requests ,"index.html")
+def about(requests):
+	return HttpResponse('we are learning django')
